c74a8bade4a4099ed57ff668c413d0431a51fcbc
Balaji <balajijagtap336@gmail.com>
2022-03-26 00:14:19 +0530

    working code done

--- src/com/example/filesearch/FileSearchApp.java
+++ src/com/example/filesearch/FileSearchApp.java
@@ -1,12 +1,24 @@
 package com.example.filesearch;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class FileSearchApp {
 
 	String path;
 	String regex;
 	String zipFileName;
+	Pattern pattern;
+	List<File> files = new ArrayList<File>();
 
 	public static void main(String[] args) {
 		FileSearchApp app = new FileSearchApp();
@@ -23,11 +35,21 @@ public class FileSearchApp {
 			app.setPath(args[0]);
 		}
 
+		app.printArgs();
+
 		try {
 			app.walkDirectory(app.getPath());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	private void printArgs() {
+		System.out.println("arguments received: ");
+		System.out.println("path: " + path);
+		System.out.println("regex: " + regex);
+		System.out.println("zip file name: " + zipFileName);
+		System.out.println("####################");
 	}
 
 	public void walkDirectory(String path) {
@@ -41,18 +63,84 @@ public class FileSearchApp {
 				processFile(file);
 			}
 		}
+
+		if (getZipFileName() != null) {
+			try {
+				addFilesToZip();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void addFilesToZip() throws IOException {
+		FileOutputStream fos = new FileOutputStream(getZipFileName());
+		ZipOutputStream zos = new ZipOutputStream(fos);
+		File dir = new File(path);
+		for (File file : files) {
+
+			String fileName = getRelativePath(file, dir);
+
+			System.out.println("Writing '" + fileName + "' to zip file");
+
+			File addfile = new File(fileName);
+			FileInputStream fis = new FileInputStream(addfile);
+			ZipEntry zipEntry = new ZipEntry(fileName);
+			zos.putNextEntry(zipEntry);
+
+			byte[] bytes = new byte[1024];
+			int length;
+			while ((length = fis.read(bytes)) >= 0) {
+				zos.write(bytes, 0, length);
+			}
+
+			zos.closeEntry();
+			fis.close();
+		}
+
+		zos.close();
+		fos.close();
+	}
+
+	private String getRelativePath(File file, File base) {
+		String file_path = file.getAbsolutePath();
+		String base_path = base.getAbsolutePath();
+		String relative = new File(base_path).toURI().relativize(new File(file_path).toURI()).getPath();
+		return relative;
 	}
 
 	public void processFile(File file) {
-		System.out.println(file.getAbsolutePath());
+		if (searchFile(file)) {
+			files.add(file);
+		}
 	}
 
-	public void searchFile(File file) {
-		System.out.println("searchFile: " + file);
+	public boolean searchFile(File file) {
+		boolean found = false;
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		while (scanner.hasNextLine()) {
+			found = searchText(scanner.nextLine());
+			if (found) {
+				break;
+			}
+		}
+
+		scanner.close();
+		return found;
+	}
+
+	public boolean searchText(String line) {
+		return (getRegex() == null) ? true : pattern.matcher(line).matches();
 	}
 
 	public void addFileToZip(File file) {
-		System.out.println("addFileToZip: " + file);
+		System.out.println(file.getAbsolutePath() + " added to zip");
 	}
 
 	public String getPath() {
@@ -69,6 +157,7 @@ public class FileSearchApp {
 
 	public void setRegex(String regex) {
 		this.regex = regex;
+		this.pattern = Pattern.compile(regex);
 	}
 
 	public String getZipFileName() {
